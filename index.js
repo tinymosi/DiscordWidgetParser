3cafbb02dd08427ace29778bd207837e43235cff
M0slike <talashmanovhm@gmail.com>
2019-07-16 11:49:28 +0500

    Added functions getStatus, getGame, getDeaf, getMute and getUserInfo

--- index.js
+++ index.js
@@ -16,6 +16,76 @@ const serverName = json.name;
 var inRoom = [];
 var notInRoom = [];
 
+// return status emoji of a user
+var getStarus = function (user) {
+    if (user.game !== undefined) {
+        if (user.game.name == 'Spotify') {
+            return ('🎶 ')
+        } else {
+            return ('🤖 ')
+        }
+    }
+    
+    switch (user.status) {
+        case 'online':
+            return('💚 ');
+        case 'idle':
+            return('💛 ');
+        case 'dnd':
+            return('❤️ ');
+        default:
+            return('🤔 ')
+    }
+}
+
+// return a string with a game that user is laying right now
+var getGame = function(user) {
+    if (user.game !== undefined) {
+        switch (user.game.name) {
+            case 'Counter-Strice: Global Offensive':
+                return (' играет в CS:GO');
+            case "PLAYERUNKNOWN'S BATTLEGROUNDS":
+                return (' играет в PUBG');
+            case 'Spotify':
+                return (' слушает ' + user.game.name)
+            default:
+                return (' играет в ' + user.game.name);
+        }
+    } else return('');
+}
+
+// return emoji if user is deafed
+var getDeaf = function(user) {
+    if (user.deaf !== undefined || user.self_deaf !== undefined) {
+        if (user.deaf == true) {
+            return ('🔇');
+        } else if (user.self_deaf == true) {
+            return ('🙉');
+        } else return('');
+    } else return('');
+}
+
+// return emoji if user is muted
+var getMute = function(user) {
+    if (user.mute !== undefined || user.self_mute !== undefined) {
+        if (user.mute == true) {
+            return ('🤐');
+        } else if (user.self_mute == true) {
+            return ('🙊');
+        } else return('');
+    } else return ('');
+}
+
+// return full info about user
+var getUserInfo = function(user) {
+    let member ='';
+    if (user.nick !== undefined) {
+        return member += getDeaf(user) + getMute(user) + getStarus(user) + user.nick + ' (' + user.username + ')' + getGame(user)
+    } else {
+        return member += getDeaf(user) + getMute(user) + getStarus(user) + user.username + getGame(user)
+    }
+}
+
 // split all the members in two arrays.
 // inRoom - contains all of the users that in a voice chat room right now.
 // notInRoom - contains the rest of users that are online but not in any voice chat room.
@@ -28,12 +98,10 @@ for (member of members) {
     }
 }
 
-log(notInRoom);
-log(inRoom);
-
-// delimiter
+log('Arrays counter:')
+log('inRoom: ' + inRoom);
+log('notInRoom: ' + notInRoom);
 log('---')
-log('In rooms:')
 
 // getting an array of users in rooms
 var roomsContent = [];
@@ -45,31 +113,29 @@ for (channel of channels) {
         var memberId = user.channel_id;
 
         if (channelId == memberId) {
-            room.push(user.username); // here you'll get all the needed information about user. It's just a username for now as a proof of concept.
+            room.push(getUserInfo(user));
         }
     }
 
     if (room.length != 0) {
         room.unshift(channel.name, '---');
+        room.push('\n');
         roomsContent.push(room)
     }
 }
 
-// log to console contet of an arrays of users in rooms
-for (item of roomsContent) {
-    log(item);
-}
-
-
 // getting an array of users that not in any room
 var onlineUsers = [];
 for (user of notInRoom) {
-    onlineUsers.push(user.username); // here you'll get all the needed information about user. It's just a username for now as a proof of concept.
+    onlineUsers.push(getUserInfo(user)); // here you'll get all the needed information about user. It's just a username for now as a proof of concept.
 }
 
-// delimiter
+// log to console content of an arrays of users in rooms
+log('In rooms:')
+for (item of roomsContent) {
+    log(item);
+}
 log('------')
+// log to console content of an arrays of users not in rooms
 log('Not in any room:')
-
-// log to console contet of an arrays of users not in rooms
-log(onlineUsers)
+log(onlineUsers.join(', '))
